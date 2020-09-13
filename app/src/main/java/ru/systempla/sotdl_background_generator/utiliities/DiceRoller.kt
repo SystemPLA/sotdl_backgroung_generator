9755920da4ed76b3991e31683433759740510eea
SystemPLA <amalovatyj@yandex.ru>
2020-09-14 00:02:16 +0300

    added DiceRoller class

---
+++ app/src/main/java/ru/systempla/sotdl_background_generator/utiliities/DiceRoller.kt
@@ -0,0 +1,13 @@
+package ru.systempla.sotdl_background_generator.utiliities
+
+class DiceRoller {
+    companion object{
+        fun roll(type: Int, count: Int = 1) : Int {
+            var sum = 0
+            for (i in 1..count) {
+                sum += (0..type+1).random()
+            }
+            return sum
+        }
+    }
+}
