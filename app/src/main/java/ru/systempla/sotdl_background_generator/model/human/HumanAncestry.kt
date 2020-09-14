982316cbdbd590910f6b62e117eb98d7c9d905da
SystemPLA <amalovatyj@yandex.ru>
2020-09-14 05:26:11 +0300

    HumanAncestry Builder

--- app/src/main/java/ru/systempla/sotdl_background_generator/model/human/HumanAncestry.kt
+++ app/src/main/java/ru/systempla/sotdl_background_generator/model/human/HumanAncestry.kt
@@ -1,7 +1,35 @@
 package ru.systempla.sotdl_background_generator.model.human
 
 import ru.systempla.sotdl_background_generator.model.Ancestry
+import ru.systempla.sotdl_background_generator.model.GenerationTable
+import ru.systempla.sotdl_background_generator.utiliities.DiceRoller
 
-class HumanAncestry : Ancestry() {
+class HumanAncestry private constructor(private var name : String = "-", private val features: Map<String, String>): Ancestry() {
 
+    private constructor(builder: Builder) : this(builder.name, builder.features) {
+        this.featuresMap = features
+        this.characterName = name
+    }
+
+    class Builder{
+        var features: LinkedHashMap<String, String> = LinkedHashMap()
+        var name: String = ""
+
+        fun newFeature(featureGenerationTable : GenerationTable) : Builder {
+            features[featureGenerationTable.name] =
+                featureGenerationTable.generateProperty(DiceRoller.roll(
+                    featureGenerationTable.usedDie[0],
+                    featureGenerationTable.usedDie[1]
+                )
+            )
+            return this
+        }
+
+        fun setName(name: String) : Builder {
+            this.name = name
+            return this
+        }
+
+        fun create(): HumanAncestry = HumanAncestry(this)
+    }
 }
