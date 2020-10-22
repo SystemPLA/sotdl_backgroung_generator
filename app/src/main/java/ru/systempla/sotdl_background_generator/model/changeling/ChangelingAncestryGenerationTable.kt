7eadf9911260b2cf0b166553809aa64d81f30f46
SystemPLA <amalovatyj@yandex.ru>
2020-10-22 11:06:17 +0300

    correction

--- app/src/main/java/ru/systempla/sotdl_background_generator/model/changeling/ChangelingAncestryGenerationTable.kt
+++ app/src/main/java/ru/systempla/sotdl_background_generator/model/changeling/ChangelingAncestryGenerationTable.kt
@@ -7,14 +7,10 @@ class ChangelingAncestryGenerationTable : GenerationTable {
     override var usedDie = intArrayOf(3,6)
     override fun generateProperty(inputParameter: Int): String {
         return when (inputParameter) {
-            3, 4 -> "You appear to be a goblin. Go to the Goblin ancestry " +
-                    "entry to determine your age, build, and appearance."
-            in 5..7 -> "You appear to be a dwarf. Go to the Dwarf ancestry " +
-                    "entry to determine your age, build, and appearance."
-            in 8..15 -> "You appear to be a human. Go to the Human ancestry " +
-                    "entry to determine your age, build, and appearance."
-            16, 17 -> "You appear to be an orc. Go to the Orc ancestry entry " +
-                    "to determine your age, build, and appearance."
+            3, 4 -> "You appear to be a goblin."
+            in 5..7 -> "You appear to be a dwarf."
+            in 8..15 -> "You appear to be a human."
+            16, 17 -> "You appear to be an orc."
             18 -> "The GM determines your ancestry, age, build, and appearance."
             else -> "!!! Error: Wrong input parameter !!!"
         }
